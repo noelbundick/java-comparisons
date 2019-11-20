b9f7873c2653783374daf2aa85cdfddae9592430
Noel Bundick <noelbundick@gmail.com>
2019-11-20 15:26:07 -0800

    Use new Azure Storage SDK

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # comparisons
+
+```shell
+mvn spring-boot:run
+```
+
+## Configuration
+
+Environment variables for full functionality:
+
+* `AZURE_STORAGE_CONN_STRING`: A connection string for an Azure Storage Account
---
+++ src/main/java/com/noelbundick/comparisons/StorageHandler.java
@@ -0,0 +1,58 @@
+package com.noelbundick.comparisons;
+
+import com.azure.storage.blob.BlobContainerAsyncClient;
+import com.azure.storage.blob.BlobServiceAsyncClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobContainerItem;
+import com.azure.storage.blob.models.BlobItem;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+public class StorageHandler {
+    private static ParameterizedTypeReference<Map<String, List<BlobItem>>> listAllBlobsTypeRef = new ParameterizedTypeReference<>() {};
+
+    private final BlobServiceAsyncClient serviceClient;
+
+    public StorageHandler(String connectionString) {
+        serviceClient = new BlobServiceClientBuilder()
+            .connectionString(connectionString)
+            .buildAsyncClient();
+    }
+
+    /**
+     * List all containers in a Storage Account
+     */
+    public Mono<ServerResponse> listContainers(ServerRequest request) {
+        Flux<BlobContainerItem> containers = serviceClient.listBlobContainers();
+        return ok().body(containers, BlobContainerItem.class);
+    }
+
+    /**
+     * List all blobs in all containers in a Storage Account
+     */
+    public Mono<ServerResponse> listAllBlobs(ServerRequest request) {
+        Mono<Map<String, List<BlobItem>>> results = serviceClient.listBlobContainers()
+            .flatMap(c -> {
+                String containerName = c.getName();
+                BlobContainerAsyncClient containerClient = serviceClient.getBlobContainerAsyncClient(containerName);
+                return containerClient.listBlobs().map(b -> new AbstractMap.SimpleEntry<>(containerName, b));
+            })
+            .reduce(new HashMap<>(), (map, entry) -> {
+                String containerName = entry.getKey();
+                BlobItem blob = entry.getValue();
+
+                map.computeIfAbsent(containerName, x -> new ArrayList<>())
+                    .add(blob);
+                return map;
+            });
+
+        return ok().body(results, listAllBlobsTypeRef);
+    }
+}
