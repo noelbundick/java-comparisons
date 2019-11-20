6b17b253e30ba8dbcee4233f4d82d6238cd65fea
Noel Bundick <noelbundick@gmail.com>
2019-11-20 14:08:57 -0800

    Rename application, reformat code

--- src/main/java/com/noelbundick/comparisons/Application.java
+++ src/main/java/com/noelbundick/comparisons/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ComparisonsApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComparisonsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
