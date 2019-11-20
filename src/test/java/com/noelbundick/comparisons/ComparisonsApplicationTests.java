a4482caa10f521f4771ae0aaec0e734a9f5d468c
Noel Bundick <noelbundick@gmail.com>
2019-11-20 13:12:35 -0800

    Spring Initializr: Java 11 + Maven + Spring Boot 2.2.1 + WebFlux

---
+++ src/test/java/com/noelbundick/comparisons/ComparisonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelbundick.comparisons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComparisonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
